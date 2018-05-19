a94f31c60e6232bccc4520602ed27a0694b85ec0
monkeydioude <monkeydioude@gmail.com>
2018-05-19 20:46:21 +0200

    Mistaken payload form ?

--- bitbucket.go
+++ bitbucket.go
@@ -8,24 +8,22 @@ import (
 )
 
 type Bitbucket struct {
-	Data struct {
-		Push struct {
-			Changes []struct {
-				New struct {
-					Type string `json:"type"`
-					Name string `json:"name"`
-				} `json:"new"`
-			} `json:"changes"`
-		} `json:"push"`
-		Repository struct {
-			Fullname string `json:"full_name"`
-			Links    struct {
-				Html struct {
-					Href string `json:"href"`
-				} `json:"html"`
-			} `json:"links"`
-		} `json:"repository"`
-	} `json:"data"`
+	Push struct {
+		Changes []struct {
+			New struct {
+				Type string `json:"type"`
+				Name string `json:"name"`
+			} `json:"new"`
+		} `json:"changes"`
+	} `json:"push"`
+	Repository struct {
+		Fullname string `json:"full_name"`
+		Links    struct {
+			Html struct {
+				Href string `json:"href"`
+			} `json:"html"`
+		} `json:"links"`
+	} `json:"repository"`
 }
 
 func bitbucketToPayload(r io.Reader) *Payload {
@@ -38,12 +36,12 @@ func bitbucketToPayload(r io.Reader) *Payload {
 	}
 
 	return &Payload{
-		Ref:     fmt.Sprintf("refs/heads/%s", b.Data.Push.Changes[0].New.Name),
+		Ref:     fmt.Sprintf("refs/heads/%s", b.Push.Changes[0].New.Name),
 		Action:  "push",
-		HtmlUrl: b.Data.Repository.Links.Html.Href,
+		HtmlUrl: b.Repository.Links.Html.Href,
 		Repository: Repository{
-			Name:    b.Data.Repository.Fullname,
-			HtmlUrl: b.Data.Repository.Links.Html.Href,
+			Name:    b.Repository.Fullname,
+			HtmlUrl: b.Repository.Links.Html.Href,
 		},
 	}
 }
