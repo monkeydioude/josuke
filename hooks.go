e9304476f151d91a8e92fc83980d99337ed1069c
pyxide <pyxide@pyxide.org>
2021-10-09 02:54:45 +0200

    feat: generic request handler, gogs hook implemented

--- hooks.go
+++ hooks.go
@@ -21,78 +21,79 @@ func (j *Josuke) getHook(name string) *Hook {
 	return nil
 }
 
-// GithubRequest handles github's webhook triggers
-func (j *Josuke) GithubRequest(rw http.ResponseWriter, req *http.Request) {
-	log.Printf("[INFO] Caught call from GitHub %+v\n", req.URL)
-	defer req.Body.Close()
-
-	githubEvent := req.Header.Get("x-github-event")
-	if githubEvent == "" {
-		log.Println("[ERR ] x-github-event was empty in headers")
-		return
-	}
-
-	requestSignature := req.Header.Get("x-hub-signature-256")
-	if requestSignature == "" {
-		log.Println("[ERR ] x-hub-signature-256 was empty in headers")
-		return
-	}
-
+func getBody(reader io.Reader, debug bool) (string, error) {
 	buf := new(strings.Builder)
-	_, err := io.Copy(buf, req.Body)
+	_, err := io.Copy(buf, reader)
 	if err != nil {
-		log.Printf("[ERR ] Could not read Payload. Reason: %s", err)
-		return
+		return "", err
 	}
 	s := buf.String()
 
-	if j.Debug {
+	if debug {
 		log.Printf("[DBG ] start body %d ====\n", len(s))
 		fmt.Println(s)
 		log.Println("[DBG ] end body ====")
 		//log.Println(hex.EncodeToString([]byte(s)))
 		//log.Println("[DBG ] end body as hex ====")
 	}
+	return s, nil
+}
 
-	bodyReader := ioutil.NopCloser(strings.NewReader(s))
+func (j *Josuke) GenericRequest(
+	rw http.ResponseWriter,
+	req *http.Request,
+	scmType string,
+	eventHeaderName string,
+	signatureHeaderName string) {
 
-	//log.Printf("[INFO] check signature: %s\n",  requestSignature)
-	// FIXME : should aleady be present when calling this method.
-	// hardcoded name
-	hook := j.getHook("github")
+	log.Printf("[INFO] Caught call from %s %+v\n", scmType, req.URL)
+	defer req.Body.Close()
+
+	scmEvent := req.Header.Get(eventHeaderName)
+	if scmEvent == "" {
+		log.Printf("[ERR ] %s was empty in headers\n", eventHeaderName)
+		return
+	}
+
+	hook := j.getHook(scmType)
 
 	if hook == nil {
 		log.Println("[ERR ] cannot find hook for secret")
 		return
 	}
 
-	if hook.SecretBytes == nil {
-//		log.Printf("[INFO] hook secret: %s\n", hook.Secret)
-//		secretBytes, err := hex.DecodeString(hook.Secret)
-//		if err != nil {
-//			log.Printf("[ERR ] cannot decode hex secret: %s\n", err)
-//			return
-//		}
-//		hook.SecretBytes = secretBytes
-		hook.SecretBytes = []byte(hook.Secret)
-	}
-
-	signature := hmacSha256(hook.SecretBytes, s)
-	//log.Printf("[INFO] payload signature: %s\n", signature)
-	// TODO ConstantTimeCompare to not leak information
-	if requestSignature != signature {
-		log.Printf("[ERR ] payload signature does not match:\n  request  %s\n  expected %s\n", requestSignature, signature)
+	s, err := getBody(req.Body, j.Debug)
+	if err != nil {
+		log.Printf("[ERR ] Could not read body. Reason: %s", err)
 		return
 	}
-		
+
+	if hook.SecretBytes != nil {
+		requestSignature := req.Header.Get(signatureHeaderName)
+		if requestSignature == "" {
+			log.Printf("[ERR ] %s was empty in headers\n", signatureHeaderName)
+			return
+		}
+
+		signature := hmacSha256(hook.SecretBytes, s)
+		//log.Printf("[INFO] payload signature: %s\n", signature)
+		// TODO ConstantTimeCompare to not leak information
+		if requestSignature != signature {
+			log.Printf("[ERR ] payload signature does not match:\n  request  %s\n  expected %s\n", requestSignature, signature)
+			return
+		}
+	}
+
+	bodyReader := ioutil.NopCloser(strings.NewReader(s))
+
 	payload, err := fetchPayload(bodyReader)
 
 	if err != nil {
-		log.Printf("[ERR ] Could not fetch Payload. Reason: %s", err)
+		log.Printf("[ERR ] Could not fetch payload. Reason: %s", err)
 		return
 	}
 
-	payload.Action = githubEvent
+	payload.Action = scmEvent
 
 	action, info := payload.getDeployAction(j.Deployment)
 	if action == nil {
@@ -103,6 +104,28 @@ func (j *Josuke) GithubRequest(rw http.ResponseWriter, req *http.Request) {
 	if err := action.execute(info); err != nil {
 		log.Printf("[ERR ] Could not execute action. Reason: %s", err)
 	}
+}
+
+// GogsRequest handles gogs' webhook triggers
+func (j *Josuke) GogsRequest(rw http.ResponseWriter, req *http.Request) {
+
+	scmType := "gogs"
+	// X-Gogs-Delivery
+	eventHeaderName := "x-gogs-event"
+	signatureHeaderName := "x-gogs-signature"
+
+	j.GenericRequest(rw, req, scmType, eventHeaderName, signatureHeaderName)
+}
+
+// GithubRequest handles github's webhook triggers
+func (j *Josuke) GithubRequest(rw http.ResponseWriter, req *http.Request) {
+
+	scmType := "github"
+	eventHeaderName := "x-github-event"
+	// Could be x-hub-signature for older servers.
+	signatureHeaderName := "x-hub-signature-256"
+
+	j.GenericRequest(rw, req, scmType, eventHeaderName, signatureHeaderName)
 }
 
 // BitbucketRequest handles github's webhook triggers
