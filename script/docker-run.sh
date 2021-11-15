d7c4cf05d81121ddb45327c4bf0c4b9568acbcd9
Olivier Parent <oparent@gmail.com>
2021-11-15 11:17:14 +0100

    fix: ignore Win exe, script enhancements with shellcheck

--- script/docker-run.sh
+++ script/docker-run.sh
@@ -3,13 +3,12 @@
 u_ok_jojo() {
     maxIt=4
     sleepDuration=1
-    for d in `seq 1 $maxIt`; do 
-        if [ ! $(docker ps -qf ancestor="$BIN_IMAGE_NAME") = "" ]; then
+    for _ in `seq 1 $maxIt`; do 
+        if [ ! "$(docker ps -qf ancestor="$BIN_IMAGE_NAME")" = "" ]; then
             return 0
-        else
-            printf "[INFO] container did not start yet. sleep $sleepDuration \n"
-            sleep $sleepDuration
         fi
+        printf '[INFO] container did not start yet. sleep %s\n' "$sleepDuration"
+        sleep "$sleepDuration"
     done
     return 1
 }
@@ -17,32 +16,32 @@ u_ok_jojo() {
 DEFAULT_PORT=8082
 
 # Expecting optional BIN_IMAGE_NAME env var from Makefile
-if [ -z $BIN_IMAGE_NAME ]; then
+if [ -z "$BIN_IMAGE_NAME" ]; then
     echo "[WARN] BIN_IMAGE_NAME not set. Using 'josuke' as default"
     BIN_IMAGE_NAME=josuke
 fi
 
 # Expecting mandatory CONF_FILE env var from Makefile
-if [ -z $CONF_FILE ]; then
+if [ -z "$CONF_FILE" ]; then
     echo "[ERR ] CONF_FILE not provided"
     exit 1
 fi
 
 # Checking image already has a running container
 CONTAINER_ID=$(docker ps -qf ancestor="$BIN_IMAGE_NAME")
-if [ ! $CONTAINER_ID = "" ]; then
+if [ ! "$CONTAINER_ID" = "" ]; then
     echo "[ERR ] Container with ID '$CONTAINER_ID' already running for image '$BIN_IMAGE_NAME'"
     exit 1
 fi
 
 PORT=$(jq '.port' "$CONF_FILE")
 
-if [ $PORT  = "null" ]; then
+if [ "$PORT"  = "null" ]; then
     echo "[WARN] 'port' not found in conf file $CONF_FILE, using $DEFAULT_PORT"
     PORT=$DEFAULT_PORT
 fi
 
-docker run --network="host" -d -e "CONF_FILE=$CONF_FILE" -e "PORT=$PORT" $BIN_IMAGE_NAME
+docker run --network="host" -d -e "CONF_FILE=$CONF_FILE" -e "PORT=$PORT" "$BIN_IMAGE_NAME"
 
 # checking container status
 u_ok_jojo
--- script/run.sh
+++ script/run.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-BASE_PATH=$(pwd)/
 
-if [ ! -z $DOCKER ]; then
-    BASE_PATH=/src/
+if [ -n "$DOCKER" ]; then
+    BASE_PATH=/src
+else
+    BASE_PATH=$(pwd)
 fi
 
-if [ -z $CONF_FILE ]; then
+if [ -z "$CONF_FILE" ]; then
     echo [ERR ] "A path to a config file must be provided (CONF_FILE env var)"
     exit 1
 fi
 
-cd $BASE_PATH"bin/josuke" && go build -o $GOPATH/josuke
+cd "$BASE_PATH/bin/josuke" && go build -o "$GOPATH/josuke"
 
-$GOPATH/josuke -c $BASE_PATH$CONF_FILE
+"$GOPATH/josuke" -c "$BASE_PATH$CONF_FILE"
