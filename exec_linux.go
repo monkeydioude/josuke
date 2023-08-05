88d667c7d13fced49b1639401ebbfc93724dd848
mkd <monkeydioude@gmail.com>
2023-08-05 18:19:26 +0200

    better logging

--- exec_linux.go
+++ exec_linux.go
@@ -1,16 +1,23 @@
 package josuke
 
 import (
+	"bytes"
+	"fmt"
 	"os/exec"
 	"syscall"
 )
 
 // NativeExecuteCommand executes a command with a specific user.
 func NativeExecuteCommand(cmd *exec.Cmd) error {
-	cmd.SysProcAttr = &syscall.SysProcAttr{}
-	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: currentUser.Uid, Gid: currentUser.Gid}
+	var stderr bytes.Buffer
+	var stdout bytes.Buffer
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Credential: &syscall.Credential{Uid: currentUser.Uid, Gid: currentUser.Gid},
+	}
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
 	if err := cmd.Run(); err != nil {
-		return err
+		return fmt.Errorf("%s: %s", err, cmd.Stderr)
 	}
 	return nil
 }
--- exec_windows.go
+++ exec_windows.go
@@ -1,13 +1,19 @@
 package josuke
 
 import (
+	"bytes"
+	"fmt"
 	"os/exec"
 )
 
 // NativeExecuteCommand executes a command.
 func NativeExecuteCommand(cmd *exec.Cmd) error {
+	var stderr bytes.Buffer
+	var stdout bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
 	if err := cmd.Run(); err != nil {
-		return err
+		return fmt.Errorf("%s: %s", err, cmd.Stderr)
 	}
 	return nil
 }
