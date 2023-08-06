5859c07ee76b3fa019ac99e0a327a8de2b9bca42
mkd <monkeydioude@gmail.com>
2023-08-06 18:07:11 +0200

    trying no set groups

--- exec_linux.go
+++ exec_linux.go
@@ -8,7 +8,11 @@ import (
 // NativeExecuteCommand executes a command with a specific user.
 func NativeExecuteCommand(cmd *exec.Cmd) error {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Credential: &syscall.Credential{Uid: currentUser.Uid, Gid: currentUser.Gid},
+		Credential: &syscall.Credential{
+			Uid: currentUser.Uid,
+			Gid: currentUser.Gid,
+			NoSetGroups: true,
+		},
 	}
 	if err := cmd.Run(); err != nil {
 		return err
